7354fef456afe1a2690eec7744e1b9c6f4866386
Yuki Fujii <yuki.fujii@hotmail.com>
2016-10-06 15:26:35 -0300

    Start program media

---
+++ media.elm
@@ -0,0 +1,105 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.App as App
+import String
+
+
+-- model
+
+
+type alias Model =
+    { resultado : Float
+    , notas : List Float
+    , nota : Float
+    , qtd : Int
+    , situacao : String
+    , error : Maybe String
+    }
+
+
+initModel : Model
+initModel =
+    { resultado = 0
+    , notas = []
+    , nota = -1
+    , qtd = 0
+    , situacao = ""
+    , error = Nothing
+    }
+
+
+
+-- update
+
+
+type Msg
+    = Nota String
+    | Adicionar
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Nota nota ->
+            case String.toFloat nota of
+                Ok input ->
+                    { model
+                        | nota = input
+                        , error = Nothing
+                    }
+
+                Err err ->
+                    { model
+                        | nota = -1
+                        , error = Just err
+                    }
+
+        Adicionar ->
+            let
+                newNotas =
+                    model.nota :: model.notas
+            in
+                { model
+                    | notas = newNotas
+                    , nota = -1
+                    , qtd = model.qtd + 1
+                }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div []
+            [ text "Insira uma nota: "
+            , input
+                [ type' "text"
+                , onInput Nota
+                , value
+                    (if model.nota == -1 then
+                        ""
+                     else
+                        toString model.nota
+                    )
+                ]
+                []
+            , button
+                [ type' "button"
+                , onClick Adicionar
+                ]
+                [ text "Adicionar" ]
+            ]
+        , div []
+            [ text ("Quantidade de notas inseridas: " ++ (toString model.qtd)) ]
+        ]
+
+
+main : Program Never
+main =
+    App.beginnerProgram
+        { model = initModel
+        , update = update
+        , view = view
+        }
